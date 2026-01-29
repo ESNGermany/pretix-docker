bbd2539b878073bd75b93ad68a3f6eeffc8086b2
Magnus Johannes Schröder <magnus.schroeder@esnhamburg.de>
2026-01-29 20:05:12 +0100

    Improve backup

--- restic/scripts/backup.sh
+++ restic/scripts/backup.sh
@@ -1,2 +1,7 @@
+while ! pg_isready -q; do
+  echo "Postgres is not ready yet…"
+  sleep 1
+done
+
 restic backup --tag db --stdin-filename db.dump --stdin-from-command -- pg_dump -Fc
 restic backup --tag app /data
--- restic/scripts/restore.sh
+++ restic/scripts/restore.sh
@@ -1,6 +1,7 @@
-while ! pg_isready -h postgres -q; do
+while ! pg_isready -q; do
+  echo "Postgres is not ready yet…"
   sleep 1
 done
 
-restic dump --tag db latest db.dump | pg_restore
+restic dump --tag db latest db.dump | pg_restore -d "$PGDATABASE"
 restic restore --tag app latest --target /app
